dee5426671783a836589e389c398522217c52a79
ain-soph <ain-soph@live.com>
2021-02-12 00:57:38 -0500

    add a new cmd argument

--- extract_wav.py
+++ extract_wav.py
@@ -10,8 +10,6 @@ from wave import Wave_write
 
 start_encode = 'SND '.encode()
 wav_params = (1, 2, 44100, 0, 'NONE', 'NONE')
-silence_interval_sec = 0.5
-num_bytes = int(wav_params[1]*wav_params[2]*silence_interval_sec)
 
 
 def parse_args(args=None):  # : list[str]
@@ -19,15 +17,20 @@ def parse_args(args=None):  # : list[str]
     parser = argparse.ArgumentParser()
     parser.add_argument('--src-path', help='source ddb file path')
     parser.add_argument('--dst-path',
-                        help='destination extract path, default="./wav.zip (merge.wav)"')
+                        help='destination extract path, default to be"./wav.zip (merge.wav)"')
     parser.add_argument('--merge', help='enable to generate a merged large wav file',
                         action='store_true')
+    parser.add_argument('--silence-interval', help='silence interval seconds, default to be 0',
+                        type=float, default=0.0)
 
     # parse args
     args = parser.parse_args(args)
     src_path: str = os.path.normpath(args.src_path)
     dst_path: str = args.dst_path
     merge: bool = args.merge
+    silence_interval: float = args.silence_interval
+    num_bytes = int(wav_params[1]*wav_params[2]*silence_interval)
+
     if dst_path is None:
         dst_path = './merge.wav' if merge else './wav.zip'
     elif merge and not os.path.isdir(dst_path):
@@ -40,11 +43,12 @@ def parse_args(args=None):  # : list[str]
         dir_path = os.path.dirname(dst_path)
     if dir_path and not os.path.exists(dir_path):
         os.makedirs(dir_path)
-    return src_path, dst_path, merge
+
+    return src_path, dst_path, merge, num_bytes
 
 
 def main():
-    src_path, dst_path, merge = parse_args()
+    src_path, dst_path, merge, num_bytes = parse_args()
     with open(src_path, 'rb') as ddb_f:
         ddb_data = ddb_f.read()
     length = len(ddb_data)
@@ -84,7 +88,7 @@ def main():
 
         if merge:
             merge_f.writeframes(pcm_data)
-            # merge_f.writeframes(b'\x00'*num_bytes)
+            merge_f.writeframes(b'\x00'*num_bytes)
         else:
             bytes_f = io.BytesIO()
             file_path = f'wav/{file_id}.wav'
